ae1a91e440c99da7abd3b3184f44456c6bdb7e06
yauhen.trzhensimekh <yauhen.trzhensimekh@havi.com>
2023-02-16 12:18:23 +0200

    Fix for Spring tests

--- src/test/java/com/naya/oracledbpocapplication/OracleDbPocApplicationTests.java
+++ src/test/java/com/naya/oracledbpocapplication/OracleDbPocApplicationTests.java
@@ -3,9 +3,7 @@ package com.naya.oracledbpocapplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.naya.oracledbpocapplication.models.Product;
 import com.naya.oracledbpocapplication.repositories.ProductRepository;
-import com.naya.oracledbpocapplication.services.ProductService;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -33,15 +31,13 @@ class OracleDbPocApplicationTests {
     private MockMvc mockMvc;
 
     @Autowired
-    private ProductService productServiceRepo;
-    @Autowired
     private ProductRepository productRepository;
 
     private ResultActions resultActions;
 
     @RepeatedTest(5)
     public void testSaveProduct() throws Exception {
-    long number = new Random().nextInt(200);
+    long number = new Random().nextInt(300);
     Product product = new Product((Long)number , "TestProduct"+number, null, null);
     System.out.println("---->"+asJsonString(product));
         CountDownLatch latch = new CountDownLatch(1);
